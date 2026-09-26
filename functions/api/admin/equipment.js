76eb86b87cdf9df8f0138cca8c8fb82db71757b4
czepepe-dev <czepepe@gmail.com>
2026-09-26 22:30:58 +0200

    Update equipment.js

--- functions/api/admin/equipment.js
+++ functions/api/admin/equipment.js
@@ -1,9 +1,16 @@
+```javascript
 import {auth,json,gh,b64decode,b64encode,BRANCH} from "./_shared.js";
 
-export function onRequestGet({request,env}) {
-  return new Response("EQUIPMENT IMPORT FUNGUJE", {
+export async function onRequestGet({request,env}) {
+
+  const a = await auth(request,env);
+
+  if(a) return a;
+
+  return new Response("AUTH FUNGUJE", {
     headers: {
       "Content-Type": "text/plain; charset=utf-8"
     }
   });
 }
+```
